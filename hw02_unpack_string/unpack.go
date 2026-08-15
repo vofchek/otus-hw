ea882eb019c0062056ccdbc2dc330a64cd13c31e
Vladimir Kononov <vofchek@gmail.com>
2026-08-15 21:59:21 +0300

    HW2: решение для варианта с экранированием

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -17,28 +17,45 @@ func Unpack(packed string) (string, error) {
 	unpacked := strings.Builder{}
 	// предудущий символ
 	prevSymbol := ""
+	unpackedPart := ""
+	escaping := false
 
 	// по каждой руне
 	for _, r := range packed {
+		if !escaping && r == '\\' {
+			escaping = true
+			unpackedPart = prevSymbol
+			prevSymbol = ""
+			unpacked.WriteString(unpackedPart)
+			continue
+		}
+
 		// пробуем сделать цифру
-		n, err := strconv.Atoi(string(r))
+		number, notNumber := strconv.Atoi(string(r))
+
+		if escaping && notNumber != nil && r != '\\' {
+			return "", ErrInvalidString
+		}
 
 		// err пустая, если получили цифру
 		// prevSymbol пустой, если на предыдущей итерации получили цифру, тогда рубим выполнение с ошибкой
-		if err == nil && prevSymbol == "" {
+		if !escaping && notNumber == nil && prevSymbol == "" {
 			return "", ErrInvalidString
 		}
 
 		// нашли цифру
-		if err == nil {
+		if !escaping && notNumber == nil {
 			// игнорим error WriteString, он всё равно пустой
-			unpacked.WriteString(strings.Repeat(prevSymbol, n))
+			unpackedPart = strings.Repeat(prevSymbol, number)
 			prevSymbol = ""
 			// норм символ, который нужно будет повторять
 		} else {
-			unpacked.WriteString(prevSymbol)
+			unpackedPart = prevSymbol
 			prevSymbol = string(r)
 		}
+
+		unpacked.WriteString(unpackedPart)
+		escaping = false
 	}
 
 	// последний символ не обрабатывается циклом, так что добавляем руками
