c2d56c43222d5e631e0afbf63745935edeb6df0e
Vladimir Kononov <vofchek@gmail.com>
2026-08-15 18:41:02 +0300

    HW2: решение задания без обработки экранирования

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,39 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(packed string) (string, error) {
+	var unpacked = strings.Builder{}
+	var prevSymbol string
+
+	for _, r := range packed {
+		if n, err := strconv.Atoi(string(r)); err == nil {
+			if prevSymbol == "" {
+				return "", ErrInvalidString
+			}
+
+			if n > 0 {
+				_, err := unpacked.WriteString(strings.Repeat(string(prevSymbol), n))
+				if err != nil {
+					return "", err
+				}
+			}
+
+			prevSymbol = ""
+		} else {
+			unpacked.WriteString(prevSymbol)
+			prevSymbol = string(r)
+		}
+	}
+
+	if prevSymbol != "" {
+		unpacked.WriteString(prevSymbol)
+	}
+
+	return unpacked.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -18,6 +18,7 @@ func TestUnpack(t *testing.T) {
 		{input: "aaa0b", expected: "aab"},
 		{input: "🙃0", expected: ""},
 		{input: "aaф0b", expected: "aab"},
+		{input: "d\n5abc", expected: "d\n\n\n\n\nabc"},
 		// uncomment if task with asterisk completed
 		// {input: `qwe\4\5`, expected: `qwe45`},
 		// {input: `qwe\45`, expected: `qwe44444`},
