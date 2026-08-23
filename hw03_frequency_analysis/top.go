42aebeae4a5b6f0b8be79f21275702d7505b0373
Vladimir Kononov <vofchek@gmail.com>
2026-08-23 14:55:31 +0300

    HW03: решение без *

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,52 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+type entry struct {
+	key   string
+	value uint
+}
+
+func Top10(text string) []string {
+	partsFrequency := make(map[string]uint)
+
+	for _, part := range strings.Fields(text) {
+		_, inMap := partsFrequency[part]
+		if !inMap {
+			partsFrequency[part] = 0
+		}
+		partsFrequency[part]++
+	}
+
+	if len(partsFrequency) == 0 {
+		return make([]string, 0)
+	}
+
+	entries := make([]entry, 0, len(partsFrequency))
+	for key, value := range partsFrequency {
+		entries = append(entries, entry{key, value})
+	}
+
+	sort.Slice(entries, func(i int, j int) bool {
+		if entries[i].value > entries[j].value {
+			return true
+		}
+
+		if entries[i].value == entries[j].value {
+			return entries[i].key < entries[j].key
+		}
+
+		return false
+	})
+
+	result := make([]string, 0, min(10))
+
+	for i := 0; i < 10 && i < len(entries); i++ {
+		result = append(result, entries[i].key)
+	}
+
+	return result
 }
--- hw03_frequency_analysis/top_test.go
+++ hw03_frequency_analysis/top_test.go
@@ -43,6 +43,8 @@ var text = `Как видите, он  спускается  по  лестни�
 	посидеть у огня и послушать какую-нибудь интересную сказку.
 		В этот вечер...`
 
+var smallText = "word1 word2 word3"
+
 func TestTop10(t *testing.T) {
 	t.Run("no words in empty string", func(t *testing.T) {
 		require.Len(t, Top10(""), 0)
@@ -78,5 +80,14 @@ func TestTop10(t *testing.T) {
 			}
 			require.Equal(t, expected, Top10(text))
 		}
+	})
+
+	t.Run("words less than 10", func(t *testing.T) {
+		expected := []string{
+			"word1",
+			"word2",
+			"word3",
+		}
+		require.Equal(t, expected, Top10(smallText))
 	})
 }
